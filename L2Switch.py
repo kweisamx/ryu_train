9d712c84a986bc2098faedf6de063ec4e29f13d9
kweisamx <kweisamx0322@gmail.com>
2017-10-26 21:17:27 +0800

    finish the L2switch with ryu

--- L2Switch.py
+++ L2Switch.py
@@ -1,21 +1,82 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
 
 class L2Switch(app_manager.RyuApp):
     def __init__(self,*args, **kwargs):
         super(L2Switch, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def pack_in_handler(self,ev):
+    
+    def _packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
-        ofp = datapath.ofproto
-        ofp_parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        
+        # Update the MAC address Table
 
-        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
-        out = ofp_parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            actions=actions)
+        in_port = msg.match['in_port']
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id # switch ID
+        self.mac_to_port.setdefault(dpid, {}) # If dpid(key) is not in mac set, create a default(here is return {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # Learn a mac address to avoid FLOOD next time
+        self.mac_to_port[dpid][src] = in_port
+
+        # Decise the packet send port       
+        if dst in self.mac_to_port[dpid]: # If the dst(mac) in the dpid(port forward table)
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD #
+
+        actions = [parser.OFPActionOutput(out_port)]
+        
+        # Install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions) # set the match and output
+
+        # Search the MAC address ,whether in mac address table, if yes, addflow and transfer the packet
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+
         datapath.send_msg(out)
+
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
+
+        datapath.send_msg(mod)
+
+
